ea764d3b3de588cddf8696fdc4c9fb22b8350f3d
Dennis Njau <njaudennis30@gmail.com>
2024-09-16 17:40:22 +0300

    update index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes.js";
+import './index.css';
+
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
